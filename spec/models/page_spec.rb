1d018048ab9a0515f399e81b5167c7f2bb737060
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-28 01:17:57 -0300

    add tests to relation between page content

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
+  subject { described_class }
+
   context 'validations' do
     context 'with missing required fields' do
-      it { expect(Page.new.valid?).to be_falsey }
+      it { expect(subject.new.valid?).to be_falsey }
     end
 
     context 'with all required fields supplied' do
       let(:params) { { original_url: 'http://example.com/' } }
 
-      it { expect(Page.new(params).valid?).to be_truthy }
+      it { expect(subject.new(params).valid?).to be_truthy }
+    end
+  end
+
+  context 'associations' do
+    it 'has many content' do
+      assc = described_class.reflect_on_association(:contents)
+      expect(assc.macro).to eq :has_many
     end
   end
 end
