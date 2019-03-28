0d9c83f5891a1a48d2f90a1af80c0c9474325354
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-28 01:17:57 -0300

    add tests to relation between page content

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
+  subject { described_class }
+
   context 'validations' do
     context 'with missing required fields' do
-      it { expect(Content.new.valid?).to be_falsey }
-      it { expect(Content.new(kind: 'h1').valid?).to be_falsey }
-      it { expect(Content.new(value: 'Some text').valid?).to be_falsey }
+      it { expect(subject.new.valid?).to be_falsey }
+      it { expect(subject.new(kind: 'h1').valid?).to be_falsey }
+      it { expect(subject.new(value: 'Some text').valid?).to be_falsey }
     end
 
     context 'with all required fields supplied' do
-      let(:params) { { value: 'Some text', kind: 'h1' } }
+      let(:page_id) { Page.create(original_url: 'example.com').id }
+      let(:params) { { value: 'Some text', kind: 'h1', page_id: page_id } }
 
-      it { expect(Content.new(params).valid?).to be_truthy }
+      it { expect(subject.new(params).valid?).to be_truthy }
     end
 
     context 'with invalid kind enum type' do
-      it { expect { Content.new(kind: 'foo') }.to raise_error(ArgumentError, "'foo' is not a valid kind") }
+      it { expect { subject.new(kind: 'foo') }.to raise_error(ArgumentError, "'foo' is not a valid kind") }
+    end
+  end
+
+  context 'associations' do
+    it 'belongs to page' do
+      assc = described_class.reflect_on_association(:page)
+      expect(assc.macro).to eq :belongs_to
     end
   end
 end
