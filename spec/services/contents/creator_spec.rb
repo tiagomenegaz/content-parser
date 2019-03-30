63165353ca38eecee933c1cbfc42be3a83adbb2b
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-30 17:33:57 -0300

    remove skiped tests

--- spec/services/contents/creator_spec.rb
+++ spec/services/contents/creator_spec.rb
@@ -2,24 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Contents::Creator do
   subject { described_class }
+  let(:params) { { page: Page.new(original_url: 'http://example.com/') } }
 
   context 'initialization' do
     context 'with missing required fields' do
       context 'when page is missing' do
-        xit { expect { subject.new() }.not_to raise_error }
+        it { expect { subject.new({}) }.to raise_error(KeyError, 'key not found: :page') }
       end
     end
 
     context 'with all required fields supplied' do
-      let(:params) { { page: Page.new(original_url: 'http://example.com/') } }
-
-      xit { expect { subject.new(params) }.not_to raise_error }
+      it { expect { subject.new(params) }.not_to raise_error }
     end
   end
 
   context '#call' do
-    xit 'returns nil'
+    before do
+      response = [
+        {:kind=>"h1", :value=>"Example Domain"},
+        {:kind=>"a", :value=>"http://www.iana.org/domains/example"}
+      ]
+      allow_any_instance_of(Contents::Parser).to receive(:call).and_return(response)
+    end
+
+    it 'returns correct array' do
+      service  = subject.new(params)
+
+      expect {
+        service.call
+      }.to change { Content.count }.from(0).to(2)
+    end
   end
 end
-
 
--- spec/services/contents/parser_spec.rb
+++ spec/services/contents/parser_spec.rb
@@ -2,23 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Contents::Parser do
   subject { described_class }
+  let(:params) { { base_url: 'http://example.com/' } }
 
   context 'initialization' do
     context 'with missing required fields' do
       context 'when base url is missing' do
-        xit { expect { subject.new() }.not_to raise_error }
+        it {
+          expect { subject.new({}) }.to raise_error(KeyError, 'key not found: :base_url')
+        }
       end
     end
 
     context 'with all required fields supplied' do
-      let(:params) { { base_url: 'http://example.com/' } }
 
-      xit { expect { subject.new(params) }.not_to raise_error }
+      it { expect { subject.new(params) }.not_to raise_error }
     end
   end
 
   context '#call' do
-    xit 'returns nil'
+    it 'returns correct array' do
+      service  = subject.new(params)
+      expected = [
+        {:kind=>"h1", :value=>"Example Domain"},
+        {:kind=>"a", :value=>"http://www.iana.org/domains/example"}
+      ]
+      expect(service.call).to match_array(expected)
+    end
   end
 end
 
