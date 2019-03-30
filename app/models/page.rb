11643f1e0c8e02ddeae368a3d2eb6caaafa39990
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-30 17:58:03 -0300

    add url validator using regex

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ApplicationRecord
   has_many :contents
 
-  validates :original_url, presence: true
+  validates :original_url, presence: true, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/ix }
 
   after_save do
     Contents::Creator.new(page: self).call
