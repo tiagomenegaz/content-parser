6346b878a4657a3c79e618e9356f689e212a749f
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-30 17:33:57 -0300

    remove skiped tests

--- spec/services/logs/register_spec.rb
+++ spec/services/logs/register_spec.rb
@@ -6,22 +6,38 @@ RSpec.describe Logs::Register do
   context 'initialization' do
     context 'with missing required fields' do
       context 'when url is missing' do
-        xit { expect { subject.new() }.not_to raise_error }
+        it {
+          expect { subject.new({})
+        }.to raise_error(KeyError, 'key not found: :url') }
       end
 
       context 'when message is missing' do
-        xit { expect { subject.new(ulr: 'http://example.com/') }.not_to raise_error }
+        it {
+          expect { subject.new(ulr: 'http://example.com/')
+        }.to raise_error(KeyError, 'key not found: :url') }
       end
     end
 
     context 'with all required fields supplied' do
       let(:params) { { url: 'http://example.com/', message: 'Something went wrong' } }
 
-      xit { expect { subject.new(params) }.not_to raise_error }
+      it { expect { subject.new(params) }.not_to raise_error }
     end
   end
 
   context '#call' do
-    xit 'returns nil'
+    let(:params) { { url: 'http://example.com/', message: 'Something went wrong' } }
+
+    it 'returns correct array' do
+      service  = subject.new(params)
+      expected = [
+        "---"*20,
+        "The following page has content that clouldn't be parsed http://example.com/",
+        "Error Something went wrong",
+        "Registered at #{DateTime.now}",
+        "---"*20
+      ]
+      expect(service.call).to match_array(expected)
+    end
   end
 end
